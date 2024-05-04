c107fd25c9c0bd670decd49f9e1086750af86304
valmir domingues <valmirdomingues4@gmail.com>
2024-05-04 11:50:45 -0300

    tentando arrumar o js

--- index.js
+++ index.js
@@ -9,18 +9,39 @@
       console.log(data);
       var Log = data.logradouro
       var Bai = data.bairro
-      var Uf = data.localidade
+      var Uf = data.uf
+      var Cid = data.localidade
+
+
+      const address = `${Cid}, ${Bai}, ${Uf}, `; /*constante que junta todo o endereço*/
         
-        var nome = document.getElementById(`Logradouro`);
+        let nome = document.getElementById(`Logradouro`);
         nome.innerHTML = `${Log}`
         
-        var distrito = document.getElementById(`Bairro`);
+        let distrito = document.getElementById(`Bairro`);
         distrito.innerHTML = `${Bai}`
 
-        var localidade = document.getElementById(`Localidade`);
+        let localidade = document.getElementById(`Localidade`);
         localidade.innerHTML = `${Uf}`
+      
+     return address;/*Retorna o endereço completo para o escopo global, assim podendo ser usado em outra função*/
     }
     catch (error) {
         alert(error.message);
 }
  }
+
+
+async function temperatura(){
+    const lati = document.getElementById(`latitude`).value;
+    const longi = document.getElementById(`longitude`).value;
+    try{
+        const response = await fetch(`http://api.open-meteo.com/v1/forecast?latitude=${lati}&longitude=${longi}&hourly=temperature_2m`);
+        const data = await response.json();
+        alert(data);
+
+    }
+catch (error){
+    alert(error.message);
+}
+}
