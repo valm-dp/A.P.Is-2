9d1119c8d2dc235501a6bca165d696ab98928ed8
valmir domingues <valmirdomingues4@gmail.com>
2024-05-05 18:37:56 -0300

    js consertado, indo para finalização.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
  const botao = document.getElementById(`botao`);
+ const cep = document.getElementById(`CEP`).value; 
+
+
 
  async function subir(){
     const cep = document.getElementById(`CEP`).value; 
@@ -10,7 +13,7 @@
       var Log = data.logradouro
       var Bai = data.bairro
       var Uf = data.uf
-      var Cid = data.localidade
+      var Cid = data.localidade 
 
 
       const address = `${Cid}, ${Bai}, ${Uf}, `; /*constante que junta todo o endereço*/
@@ -24,7 +27,7 @@
         let localidade = document.getElementById(`Localidade`);
         localidade.innerHTML = `${Uf}`
       
-     return address;/*Retorna o endereço completo para o escopo global, assim podendo ser usado em outra função*/
+/*Retorna o endereço completo para o escopo global, assim podendo ser usado em outra função*/
     }
     catch (error) {
         alert(error.message);
@@ -33,15 +36,24 @@
 
 
 async function temperatura(){
-    const lati = document.getElementById(`latitude`).value;
-    const longi = document.getElementById(`longitude`).value;
+    await subir();
+    /*const dataehora = new Date().toLocaleString();
+    alert(dataehora);*/
+    const lati = await document.getElementById(`latitude`).value;
+    const longi = await document.getElementById(`longitude`).value;
     try{
-        const response = await fetch(`http://api.open-meteo.com/v1/forecast?latitude=${lati}&longitude=${longi}&hourly=temperature_2m`);
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lati}&longitude=${longi}&current=temperature_2m`);
         const data = await response.json();
-        alert(data);
+        console.log(data);
+        var temp = data.current.temperature_2m;
 
+        let temperatura = document.getElementById(`Temperatura`);
+        temperatura.innerHTML = `${temp}°c`;
     }
-catch (error){
+    catch (error){
     alert(error.message);
+    }
 }
-}
+/*4f7dde9c4a9c3138edd15aef6048eeb9*/
+
+  
